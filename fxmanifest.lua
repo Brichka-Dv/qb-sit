7bdcf0b647ffda1731776fa9393ea1dc0b8e8605
Brichka yassine <107205384+Brichka-Dv@users.noreply.github.com>
2022-07-19 16:21:56 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,22 @@
 
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-sit'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+client_scripts {
+	'client/main.lua'
+}
+server_scripts {
+	'server/main.lua'
+}
+dependencies { 
+  'PolyZone', 
+  'qb-target' 
+}
+
+lua54 'yes'
