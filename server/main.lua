72decc15589567620cd0597d4fbbd93244e265e9
Brichka yassine <107205384+Brichka-Dv@users.noreply.github.com>
2022-07-19 16:20:17 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,13 +1,17 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local seatsTaken = {}
+
 RegisterNetEvent('qb-sit:takePlace', function(objectCoords)
-    seatsTaken[objectCoords] = true
-    end)
+	seatsTaken[objectCoords] = true
+end)
+
 RegisterNetEvent('qb-sit:leavePlace', function(objectCoords)
-    if   seatsTaen[objectCoords] then
-      seatsTaken[objectCoords] = nil
-      end
-    end)
-QBCore.Functions.CreateCallback('qb-sit:getPlace', function(source, cd, objectCoords)
-    cd(seatsTaken[objectCoords])
-    end)
+	if seatsTaken[objectCoords] then
+		seatsTaken[objectCoords] = nil
+	end
+end)
+
+QBCore.Functions.CreateCallback('qb-sit:getPlace', function(source, cb, objectCoords)
+	cb(seatsTaken[objectCoords])
+end)
+
