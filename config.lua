4646d6723236f4e40ba5936f9a5bdcb23497f76f
Brichka yassine <107205384+Brichka-Dv@users.noreply.github.com>
2022-07-19 13:54:58 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,1 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
 
+description 'QB-sit'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+	'client/main.lua'
+}
+
+server_scripts {
+	'server/main.lua'
+}
+
+lua54 'yes'
